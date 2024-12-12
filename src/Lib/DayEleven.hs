80d3409c42cbfc3f552680ffc079abe9594da8ba
walkerrout04@gmail.com <walkerrout04@gmail.com>
2024-12-12 07:42:58 -0800

    solve 11

--- src/Lib/DayEleven.hs
+++ src/Lib/DayEleven.hs
@@ -1,43 +1,64 @@
 module Lib.DayEleven (solveDayEleven) where
 
+import qualified Data.IntMap as IntMap
+
+import Data.List (foldl')
+import Data.IntMap (IntMap)
+
 type Stone = Int
 
-type Line = [Stone]
+type Count = Int
 
-splitNumber :: Int -> [Int]
+type StoneMap = IntMap Count
+
+splitNumber :: Int -> (Int, Int)
 splitNumber n =
   let numDigits = length (show n)
       halfShift = numDigits `div` 2
       divisor = 10 ^ halfShift
       firstHalf = n `div` divisor
       secondHalf = n `mod` divisor
-  in [firstHalf, secondHalf]
+  in (firstHalf, secondHalf)
 
 countDigits :: Int -> Int
-countDigits n
-  | n < 10 = 1
-  | otherwise = 1 + countDigits (n `div` 10)
+countDigits n = length (show n)
 
 evenDigits :: Int -> Bool
 evenDigits n = even (countDigits n)
 
-evolve :: Stone -> [Stone]
+evolve :: Stone -> [(Stone, Count)]
 evolve s
-  | s == 0 = [1]
-  | evenDigits s = splitNumber s
-  | otherwise = [2024*s]
+  -- we count occurrences (snd) for our new stones
+  | s == 0 = [(1, 1)]
+  | evenDigits s =
+      let (a, b) = splitNumber s
+      in [(a, 1), (b, 1)]
+  | otherwise = [(2024 * s, 1)]
 
-blink :: Line -> Line
-blink = concatMap evolve
+blink :: StoneMap -> StoneMap
+-- this is a little weird going back to list, but it works..
+blink stones = foldl' insertEvolved IntMap.empty (IntMap.toList stones)
+  where
+    insertEvolved acc (stone, count) = foldl' (addStone count) acc (evolve stone)
+    addStone oldCount m (newStone, newCount) = 
+      IntMap.insertWith (+) newStone (oldCount * newCount) m
 
-blinks :: Int -> Line -> Line
-blinks n = head . reverse . take n . drop 1 . iterate blink
+-- Perform n blinks
+blinks :: Int -> StoneMap -> StoneMap
+blinks 0 stones = stones
+blinks n stones = blinks (n - 1) (blink stones)
 
-solvePartOne :: Line -> Int
-solvePartOne = length . blinks 25
+solvePartOne :: [Stone] -> Int
+solvePartOne input = IntMap.foldl' (+) 0 finalMap
+  where
+    initialMap = foldl' (\m stone -> IntMap.insertWith (+) stone 1 m) IntMap.empty input
+    finalMap = blinks 25 initialMap
 
-solvePartTwo :: Line -> Int
-solvePartTwo = length . blinks 75
+solvePartTwo :: [Stone] -> Int
+solvePartTwo input = IntMap.foldl' (+) 0 finalMap
+  where
+    initialMap = foldl' (\m stone -> IntMap.insertWith (+) stone 1 m) IntMap.empty input
+    finalMap = blinks 75 initialMap
 
 solveDayEleven :: IO ()
 solveDayEleven = do
@@ -46,7 +67,7 @@ solveDayEleven = do
   print $ solvePartTwo line
   print "solved day eleven..."
 
-readFileToLine :: FilePath -> IO Line
+readFileToLine :: FilePath -> IO [Stone]
 readFileToLine filePath = do
   content <- readFile filePath
   return $ map read $ words content
